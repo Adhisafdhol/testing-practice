93114ddbee59c22dd5941e6a0f474c7b2c4f8f36
Aadheuss <aadheusx@gmail.com>
2023-11-15 20:57:35 +0700

    Add reverse string function

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,12 @@
-function reverseString() {}
+function reverseString(string) {
+  const arrString = string.split("");
+  const newArr = [];
+
+  for (let i = string.length; i >= 0; i--) {
+    newArr.push(arrString[i]);
+  }
+
+  return newArr.join("");
+}
 
 export default reverseString;
