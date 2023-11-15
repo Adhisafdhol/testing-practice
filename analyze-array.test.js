de430772a707d7309dfdd49c5657d6fd2606bcd5
Aadheuss <aadheusx@gmail.com>
2023-11-16 03:05:15 +0700

    Add second test on analyze array

--- analyze-array.test.js
+++ analyze-array.test.js
@@ -8,3 +8,12 @@ test("[1,8,3,4,2,6] == {average: 4, min: 1, max: 8, length: 6}", () => {
     length: 6,
   });
 });
+
+test("[5,1,3,76,2,89, 76, 22, 6] == {average: 31.11, min: 1, max: 89, length: 9}", () => {
+  expect(analyzeArray([5, 1, 3, 76, 2, 89, 76, 22, 6])).toEqual({
+    average: 31.11,
+    min: 1,
+    max: 89,
+    length: 9,
+  });
+});
