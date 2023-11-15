8171315f28210cc993ccde9d6a2046e1a7fcb28a
Aadheuss <aadheusx@gmail.com>
2023-11-16 02:52:06 +0700

    Add analyze array function

--- analyze-array.js
+++ analyze-array.js
@@ -1,3 +1,14 @@
-function analyzeArray() {}
+function analyzeArray(arr) {
+  const average =
+    arr.reduce((accumulator, current) => accumulator + current) / arr.length;
+  const min = arr.reduce((accumulator, current) =>
+    accumulator < current ? accumulator : current
+  );
+  const max = arr.reduce((accumulator, current) =>
+    accumulator > current ? accumulator : current
+  );
+  const length = arr.length;
+  return { average, min, max, length };
+}
 
 export default analyzeArray;
