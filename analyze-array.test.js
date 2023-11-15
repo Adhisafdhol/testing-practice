f2ff571eff80c5a692765f4afbb72e2cb856a703
Aadheuss <aadheusx@gmail.com>
2023-11-16 03:12:11 +0700

    Add third test to analyze array using float and negative

--- analyze-array.test.js
+++ analyze-array.test.js
@@ -17,3 +17,12 @@ test("[5,1,3,76,2,89, 76, 22, 6] == {average: 31.11, min: 1, max: 89, length: 9}
     length: 9,
   });
 });
+
+test("[0.5, 70, 0.25, 1, 8, -1, 0] == {average: 11.25, min: -1, max: 70, length: 6}", () => {
+  expect(analyzeArray([0.5, 70, 0.25, 1, 8, -1, 0])).toEqual({
+    average: 11.25,
+    min: -1,
+    max: 70,
+    length: 7,
+  });
+});
