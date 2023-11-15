520b7dbb5f4c9033a75ca0e009aa7c1f1c9975d2
Aadheuss <aadheusx@gmail.com>
2023-11-15 20:32:26 +0700

    Use es6 import

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,4 +1,4 @@
-const capitalize = require("./capitalize");
+import capitalize from "./capitalize";
 
 test("Capitalize the first character of a string", () => {
   expect(capitalize("test")).toMatch(/^Test$/);
