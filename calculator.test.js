0509c14f43ef7341879480d75caab28629a83617
Aadheuss <aadheusx@gmail.com>
2023-11-15 21:25:26 +0700

    Add test to calculator to check for strings

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,11 @@ import calculator from "./calculator";
 test("1 + 2 is equal to 3", () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
+
+test(`1 + "lol" returns error`, () => {
+  expect(calculator.add(1, "lol")).toMatch(/^ERROR!$/);
+});
+
+test(`"lol" + 2 returns error`, () => {
+  expect(calculator.add(1, "lol")).toMatch(/^ERROR!$/);
+});
