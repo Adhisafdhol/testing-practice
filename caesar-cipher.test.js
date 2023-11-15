d982e786972ff0b80606b167ea8f89ac4dfad232
Aadheuss <aadheusx@gmail.com>
2023-11-16 00:49:56 +0700

    Add second test with punctuation to caesar cipher

--- caesar-cipher.test.js
+++ caesar-cipher.test.js
@@ -3,3 +3,7 @@ import caesarCipher from "./caesar-cipher";
 test(`"attack at dawn" shifted by 1 returns "buubdl bu ebxo"`, () => {
   expect(caesarCipher("attack at dawn", 1)).toMatch("buubdl bu ebxo");
 });
+
+test(`"attack at dawn!??" shifted by 25 "zsszbj zs czvm!??"`, () => {
+  expect(caesarCipher("attack at dawn!??", 25)).toMatch("zsszbj zs czvm!??");
+});
