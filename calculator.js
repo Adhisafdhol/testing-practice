359cc6cdd7769c644a1383127eeddc0cbab24707
Aadheuss <aadheusx@gmail.com>
2023-11-15 21:31:59 +0700

    Add subtract method and its test

--- calculator.js
+++ calculator.js
@@ -6,6 +6,13 @@ const calculator = {
 
     return a + b;
   },
+  subtract: (a, b) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      return "ERROR!";
+    }
+
+    return a - b;
+  },
 };
 
 export default calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -15,3 +15,19 @@ test(`"lol" + 2 returns error`, () => {
 test("0.1 + 0.2 is equal to 0.3", () => {
   expect(calculator.add(0.1, 0.2)).toBeCloseTo(0.3);
 });
+
+test("1 - 2 is equal to -1", () => {
+  expect(calculator.subtract(1, 2)).toBe(-1);
+});
+
+test(`1 - "lol" returns error`, () => {
+  expect(calculator.subtract(1, "lol")).toMatch(/^ERROR!$/);
+});
+
+test(`"lol" - 2 returns error`, () => {
+  expect(calculator.subtract(1, "lol")).toMatch(/^ERROR!$/);
+});
+
+test("0.1 - 0.2 is equal to 0.3", () => {
+  expect(calculator.subtract(0.1, 0.2)).toBeCloseTo(-0.1);
+});
